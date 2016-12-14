22941e1ac01218564a3b3c1c49fe0a828922ca23
srikanth holavanahalli <srikanth.holavanahalli@gmail.com>
2016-12-13 22:44:08 -0500

    bigram added

--- Sentiment.py
+++ Sentiment.py
@@ -1,3 +1,5 @@
+import re
+from collections import Counter
 from collections import namedtuple
 import numpy as np
 from sklearn.svm import SVC
@@ -6,19 +8,17 @@ from sklearn.naive_bayes import MultinomialNB, GaussianNB
 from sklearn.neural_network import MLPClassifier
 from sklearn import metrics
 from collections import defaultdict
-
 from sklearn import cross_validation
 from nltk.corpus import stopwords
 import enchant
-
+import nltk
 import sys
-import re
-from collections import Counter
 from nltk.stem.wordnet import WordNetLemmatizer
 
 cachedStopWords = stopwords.words("english")
 lmtzr = WordNetLemmatizer()
 all_data = []
+data_bigrams =[]
 train_tags = []
 sentenceId = {}
 d =enchant.Dict("en_US")
@@ -61,12 +61,11 @@ def edits2(word):
     "All edits that are two edits away from `word`."
     return (e2 for e1 in edits1(word) for e2 in edits1(e1))
 
-############################################################################################################
+################################################################################################
 
 
 
 ##### CLI input checker ###########
-
 if(len(sys.argv) < 3):
     print("Usage: python3 sentiment.py classifier model [optional text]")
     sys.exit(-1)
@@ -79,13 +78,12 @@ elif(len(sys.argv) > 3):
 
 classifier = sys.argv[1]
 feature_model = sys.argv[2]
-
 #######################################
 
 
 ##### reading movie reviews ###################
-
 DataDoc= namedtuple('DataDoc', 'tag words')
+BiDoc= namedtuple('BiDoc', 'tag bigrams')
 with open('train.tsv') as alldata:
     for line in alldata:
         sentId = line.split()[1]
@@ -93,16 +91,17 @@ with open('train.tsv') as alldata:
             label=line.split()[-1]
             train_tags.append(label)
             word_list=line.lower().split()[2:-2]
+            bigram_list = list(nltk.ngrams(word_list, 2))
             all_data.append(DataDoc(label, word_list))
+            data_bigrams.append(BiDoc(label, bigram_list))
             sentenceId[sentId] = 'true'
 train_data = all_data[:500]
-
-###############################################################
+train_bi_data = data_bigrams[:500]
+################################################
 
 
 
 ###### building workspace ( unique words )  #######
-
 def get_space(train_data):
     word_space=defaultdict(int)
     for doc in train_data:
@@ -120,13 +119,30 @@ def get_space(train_data):
 
 word_space=get_space(train_data)
 print (len(word_space))
-
 #####################################################
 
 
+############## building workspace (bi gram) #####################################
+def get_bi_space(train_bi_data):
+    bigram_space=defaultdict(int)
+    for doc in train_bi_data:
+        for pairs in doc.bigrams:
+            if pairs[0] not in cachedStopWords and pairs[1] not in cachedStopWords:
+                w0 = lmtzr.lemmatize(pairs[0])
+                w1 = lmtzr.lemmatize(pairs[1])
+                #print(type(d.check(w)))
+                '''if(d.check(w) == True):
+                    word_space[w]=len(word_space)'''
+                w = w0 + ','+ w1
+                bigram_space[w]=len(bigram_space)
+    return bigram_space
+
+bigram_space=get_bi_space(train_bi_data)
+print (len(bigram_space))
+##################################################################################
+
 
 ###########  Unigram model  ###################
-
 def get_sparse_vec(data_point, space):
     # create empty vector
     sparse_vec = np.zeros((len(space)))
@@ -142,17 +158,48 @@ def get_sparse_vec(data_point, space):
         except:
             continue
     return sparse_vec
-train_vecs= [get_sparse_vec(data_point, word_space) for data_point in train_data]
-
 ###################################################
 
-train_vecs=np.array(train_vecs)
+
+
+########### Bigram Model #####################################################
+def get_bigram_vec(data_point, space):
+    # create empty vector
+    bigram_vec = np.zeros((len(space)))
+    for pairs in set(data_point.bigrams):
+        try:
+            if pairs[0] not in cachedStopWords and pairs[1] not in cachedStopWords:
+                w0 = lmtzr.lemmatize(pairs[0])
+                w1 = lmtzr.lemmatize(pairs[1])
+                w = w0 + ','+ w1
+                '''if (d.check(w) == True):
+                    sparse_vec[space[w]]=1'''
+                bigram_vec[space[w]]=1 #is the order of bigram important
+        except:
+            continue
+    return bigram_vec
+##############################################################################
+
+
+
+########### feature model selection ########################################################
+if feature_model == 'uni':
+    train_vecs = [get_sparse_vec(data_point, word_space) for data_point in train_data]
+    train_vecs = np.array(train_vecs)
+elif feature_model == 'bi':
+    train_bi_vecs = [get_bigram_vec(data_point, bigram_space) for data_point in train_bi_data]
+    train_vecs = np.array(train_bi_vecs)
+else:
+    print("Invalid feature model")
+    sys.exit(-1)
+
 train_tags=np.array(train_tags[:500])
+##########################################################################################
 
 
 
 
-#################### classifiers ############################
+#################### classifiers ########################################################
 print("model" + " " +sys.argv[2])
 print("classifier " + sys.argv[1])
 
@@ -170,17 +217,14 @@ else:
 
 clf.fit(train_vecs, train_tags)
 print ("\nDone fitting classifier on training data...\n")
-
-###############################################################
+###########################################################################################
 
 
 if(predictFlag == 1):
     print(clf.predict(np.array(sys.argv[3:])))
 
 
-########### post classification analysis ############################
-
-
+########### post classification analysis ########################################################
 #------------------------------------------------------------------------------------------
 print ("="*50, "\n")
 print ("Results with 5-fold cross validation:\n")
@@ -194,6 +238,4 @@ print ("precision_score\t", metrics.precision_score(train_tags, predicted,averag
 print ("recall_score\t", metrics.recall_score(train_tags, predicted,average='macro'))
 print ("\nclassification_report:\n\n", metrics.classification_report(train_tags, predicted))
 print ("\nconfusion_matrix:\n\n", metrics.confusion_matrix(train_tags, predicted))
-
-
-###########################################################################
+##################################################################################################
